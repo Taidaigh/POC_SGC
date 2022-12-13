308876c6e4ac4026d649f5a7ad08a5afc97b753a
Taidaigh <pierre.dealmeida@yahoo.fr>
2022-12-13 12:35:46 +0100

    init

--- web_page/newVacationRequestOverviewForm/assets/js/filters.js
+++
@@ -1,6 +0,0 @@
-angular.module('bonitasoft.ui.extensions',[])
-    .filter('fromNow', [function (format) {
-       return function fromNow(input) {
-     	    return moment(input,format).fromNow();
-       };
-    }]);
